99425737ba34b34b5442e8c82c80de15288babd8
dadanisme <dadanisme@upi.edu>
2022-11-16 19:43:55 +0700

    delete unused files

--- src/store/actions/fetchPosts.ts
+++
@@ -1,7 +0,0 @@
-export function fetchPosts() {
-  return new Promise<{ data: any[] }>((resolve) => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => resolve({ data: json }));
-  });
-}
