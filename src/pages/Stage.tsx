3828adc54e354867a1afd217f383745abe20de61
dadanisme <dadanisme@upi.edu>
2022-11-17 00:12:58 +0700

    configuring sound control

--- src/pages/Stage.tsx
+++ src/pages/Stage.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { selectStage, setStage, reset } from "../store/slices/stage";
